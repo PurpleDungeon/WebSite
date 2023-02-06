dc82e0e4b43dfec1ed65441e7779adb71923b4ec
Simone Bergonzi <simemablak@gmail.com>
2023-02-06 23:00:56 +0100

    add bootstrap-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './img/PurpleDungeonSmallLogoVariantPurple.png'
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <Container className='App-div-right'>
+          <h1> What is Purple Dungeon? </h1>
+        </Container>
+      </body>
+      <footer>
+        <p> © 2021 Purple Dungeon </p>
+      </footer>
     </div>
   );
 }
