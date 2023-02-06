66a3ebc5186d23fd530e5473949f31f13090a056
Simone Bergonzi <simemablak@gmail.com>
2023-02-06 23:31:58 +0100

    add bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import logo from './img/PurpleDungeonSmallLogoVariantPurple.png'
+import img1 from './img/PurpleDungeonVariantPurple.png'
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import {Container, Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
@@ -10,11 +9,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <body>
-        <Container className='App-div-right'>
-          <h1> What is Purple Dungeon? </h1>
+      <div className='App-div-right'>
+        <Container fluid>
+          <Row>
+            <Col sm={7} md={7} lg={7} xl={7} xxl={7}>
+              <img src={img1} alt="img1"></img>
+            </Col>
+            <Col sm={5} md={5} lg={5} xl={5} xxl={5}>
+              <h1> What is Purple Dungeon? </h1>
+              <p class="text-center">
+                Purple Dungeon is a spiritual folder, where Murasaki Simema (aka Simone Bergonzi) put his personal project
+              </p>
+            </Col>
+          </Row>
         </Container>
-      </body>
+      </div>
       <footer>
         <p> © 2021 Purple Dungeon </p>
       </footer>
