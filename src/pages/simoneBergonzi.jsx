d2a3983a20d8ce754a4b793b4fec848b9909296b
Simone Bergonzi <simemablak@gmail.com>
2023-07-02 23:35:09 +0200

    Trying some images

--- src/pages/simoneBergonzi.jsx
+++ src/pages/simoneBergonzi.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import { Col, Container, Image, Navbar, Row } from "react-bootstrap";
+import img1 from "./img/simoneBergonzi/questions-bro.svg"
+import img2 from "./img/simoneBergonzi/college-project-bro.svg"
+import img3 from "./img/simoneBergonzi/writing-room-bro.svg"
 
 function App() {
   return (
     <div className="App">
       <Navbar className='App-navbar'>
         <Container fluid>
-          <Navbar.Brand href="/"> 
+          <Navbar.Brand href="/">
             <button className="nav-neon-button"><span>HOME</span><i></i></button>
           </Navbar.Brand>
         </Container>
@@ -14,23 +17,58 @@ function App() {
       <div className="App-screen-black d-flex align-items-center justify-content-center">
         <Container>
           <h1>Meet Simone</h1>
-          <p>
-            Simone Bergonzi is a 21-year-old full-stack developer proficient in Javascript/Typescript, C#, and experienced with Vue, NodeJs, and Unity. 
-            He enjoys RPG video games and find inspiration in anime and manga. 
-            With a diverse set of skill and a passion for technology and interactive storytelling, Simone brings creativity and analytical thinking to their work, constantly striving for growth in the software development field.
-          </p>
+          <Row className="d-flex align-items-center justify-content-center">
+            <Col>
+              <p>
+                Simone Bergonzi is a 21-year-old full-stack developer proficient in Javascript/Typescript, C#, and experienced with Vue, NodeJs, and Unity.
+                He enjoys RPG video games and find inspiration in anime and manga.
+                With a diverse set of skill and a passion for technology and interactive storytelling, Simone brings creativity and analytical thinking to their work, constantly striving for growth in the software development field.
+              </p>
+            </Col>
+            <Col>
+              <Image src={img1} style={{height: '100%'}}/>
+            </Col>
+          </Row>
         </Container>
       </div>
-      <div className="App-screen-purple d-flex align-items-center justify-content-center">
+      <div className="App-screen-light-purple d-flex align-items-center justify-content-center">
         <Container>
-          <h1>Study and Career Path</h1>
-          <p>
-            Simone holds a Secondary School Diploma in IT from ISII Marconi with a score of 90. 
-            He had made an internship at Amada Italia, where they developed a Windows Application in C# to manage internal files. 
-            Currently employed as a full-stack developer at a local software house, specialized in Javascript/Typescript, working with NodeJS and frameworks like Vue. 
-            Alongside their work, they are pursuing a degree in Computer Science at UNIPR, further expanding their knowledge and skills. 
-            Simone is dedicated to growth and poised for a successful career in computer science.
-          </p>
+          <Col>
+            <h1>Study and Career Path</h1>
+            <Row className="d-flex align-items-center justify-content-center">
+              <Col>
+                <Image src={img2} style={{height: '100%'}}/>
+              </Col>
+              <Col>
+                <p>
+                  Simone holds a Secondary School Diploma in IT from ISII Marconi with a score of 90.
+                  He had made an internship at Amada Italia, where they developed a Windows Application in C# to manage internal files.
+                  Currently employed as a full-stack developer at a local software house, specialized in Javascript/Typescript, working with NodeJS and frameworks like Vue.
+                  Alongside their work, they are pursuing a degree in Computer Science at UNIPR, further expanding their knowledge and skills.
+                  Simone is dedicated to growth and poised for a successful career in computer science.
+                </p>
+              </Col>
+            </Row>
+          </Col>
+        </Container>
+      </div>
+      <div className="App-screen-black d-flex align-items-center justify-content-center">
+        <Container>
+          <h1>Personal Passions and Hobbies</h1>
+          <Row>
+            <Col>
+              <p>
+                Beyond the world of software development, Simone leads a vibrant and fulfilling personal life. 
+                He is an avid lover of RPG video games, finding inspiration in his immersive storytelling and intricate world-building. 
+                In his downtime, Simone can often be found exploring virtual realms and embarking on epic quests. 
+                Additionally, he draws inspiration from anime and manga, appreciating the artistry and narrative depth found in these mediums. 
+                Whether it's uncovering hidden treasures in a game or diving into the captivating narratives of anime and manga, Simone's personal passions invigorate his creativity and fuel his drive for innovative problem-solving allowing him to bring a unique perspective to his work as a developer.
+              </p>
+            </Col>
+            <Col>
+              <Image src={img3} style={{width: '100%'}}/>
+            </Col>
+          </Row>
         </Container>
       </div>
     </div>
